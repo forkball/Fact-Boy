00f7833f6c0902581761103ce5218a33b1cd98c6
ForkBombGIT <erosdipede@gmail.com>
2019-08-29 12:05:12 -0400

    Add greeting based on time of day
    Add skeleton
    Add styling
    Add dynamic link references to static
    Add empty js file for handling search requestions (GE)

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,19 @@
 from flask import render_template
+from datetime import datetime
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title='Fact Boy')
+    currentTime = datetime.now().hour
+    greetings = ["Evenin' Fact Boy","Afternoon Fact Boy","Mornin' Fact Boy"]
+    greeting = ""
+
+    if (currentTime >= 20):
+        greeting = greetings[0]
+    if (currentTime >= 12):
+        greeting = greetings[1]
+    else:
+        greeting = greetings[2]
+
+    return render_template('index.html', title='Fact Boy',greeting = greeting)
